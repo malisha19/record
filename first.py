299b896ce579eb2a7fb47585ddb3a3e7e9f04618
malisha19 <826456740@qq.com>
2022-06-27 14:10:54 +0800

    python基础

---
+++ first.py
@@ -0,0 +1,14 @@
+# from os import listxattr
+# print("hello!")
+# print('lisa','is','a','girl')
+# name = input()
+# print('hello',name)
+# name = input('please tell me your name :')
+# print('hello',name )
+# a = 1024*768
+# print(a)
+# a = 100
+# if a >= 0:
+#     print(a)
+# else:
+#     print(-a)
