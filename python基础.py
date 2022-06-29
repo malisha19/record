e19ac8f8754339a0480ee97b9e4ce8af430cdede
malisha19 <826456740@qq.com>
2022-06-29 14:36:52 +0800

    字符串和编码

--- python基础.py
+++ python基础.py
@@ -17,9 +17,6 @@
 # 空值用None表示。None不能理解为0，因为0是有意义的，而None是一个特殊的空值。
 
 # 变量
-from re import A
-
-
 # a = 123 #a是整数
 # print(a)
 # a ='abc'#a是字符串
@@ -36,3 +33,60 @@ from re import A
 # 还有一种是//，即为地板除，两个数相除仍是整数
 # 10//3 结果为3
 # %为余数运算，如：10%3，结果为1
+
+#字符串和编码
+# print('包含中文的str')
+
+# q = ord('A')
+# w = ord('中')
+# print(w) ord()函数获取字符的整数表示
+
+# chr(66)
+# chr(25991)chr()函数把编码转成对应的字符
+
+# 'ABC'.encode('ascii')输出结果为 b'ABC'
+# '中文'.encode('utf-8')输出结果为 b'\xe4\xb8\xad\xe6\x96\x87'
+# Unicode表示的str通过encode()方法可以编码为指定的bytes
+# str 转化为bytes 用encode()
+
+# bytes变为str，用decode()
+# b'ABC'.decode('ascii') 输出结果为'ABC'
+#  b'\xe4\xb8\xad\xe6\x96\x87'.decode('utf-8') 输出结果为'中文'
+
+# 如果bytes中包含无法解码的字节，decode()方法会报错
+# 如果bytes中只有一小部分无效的字节，可以传入errors='ignore'忽略错误的字节：
+# b'\xe4\xb8\xad\xff'.decode('utf-8', errors='ignore')
+# 输出结果则为'中'
+
+# len()函数 可以计算str包含多少个字符；
+# 如：len('ABC'),len('中文')
+
+# len()函数也可计算bytes字节数；
+# 如：len(b'ABC');len(b'\xe4\xb8\xad\xe6\x96\x87');len('中文'.encode('utf-8'))
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# print('hello,%s'%'lisa') %表示格式化字符串
+# %s表示用字符串替换，%d表示用整数替换，
+# 有几个%?占位符，后面就跟几个变量或者值，顺序要对应好。如果只有一个%?，括号可以省略。
+# print('hi,%s,you have %d元'%('lisa',10000))
+# print('%d-%01d' % (3, 1))
+# print('%.f' % 3.1415926)
+# 'Age: %s. Gender: %s' % (25, True) %s永远起作用，可以将任何数据类型转化成字符
+# 'growth rate: %d %%' % 7  %%来表示一个%
+
+# format()
+# print('hello,{0},你的成绩提升了{1:.2f}绩点,上升了{2}名'.format('小明',13.234,3))
+
+# f-string
+#  r = 2.5
+# >>> s = 3.14 * r ** 2
+# >>> print(f'The area of a circle with radius {r} is {s:.2f}')
+# The area of a circle with radius 2.5 is 19.62
+
+# s1 = 72
+# s2 = 85 
+# r = (s2-s1)/s1*100
+# print('小明成绩提升的百分点为%.1f%%'%r)
+# print('小明成绩提升的百分比为{0:.1f}%'.format(r))
+# print(f'小明成绩提升的百分比为{r:.1f}%')
